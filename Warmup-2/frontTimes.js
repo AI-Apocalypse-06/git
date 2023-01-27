f243d0f1128fb52cc69418e5e864944d65c68f3a
Abdurakhmon Ibragimov <117644311+AI-Apocalypse-06@users.noreply.github.com>
2023-01-27 09:19:32 +0500

    Update frontTimes.js

--- Warmup-2/frontTimes.js
+++ Warmup-2/frontTimes.js
@@ -1,6 +1,6 @@
 function frontTimes(str, n) {
   let temp = "";
-  for (let i = 1; i < n+1; i++) {
+  for (let i = 1; i <= n; i++) {
     if (str.length > 3) {
       temp += str.substring(0, 3);
     } else temp += str;
